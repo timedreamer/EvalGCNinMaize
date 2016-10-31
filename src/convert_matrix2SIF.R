650d0f02227326a50e2844619e9e6588f25b22fe
JHuang <hjraikkonen@gmail.com>
2016-10-31 09:52:43 -0400

    add diag=0 for convert_matrix2SIF.R

--- src/convert_matrix2SIF.R
+++ src/convert_matrix2SIF.R
@@ -1,4 +1,5 @@
 # This file is to cnovert correlation matrix file to SIF file.
+# For correlation, change diag to 0 to avoid self-connections.
 # The resulting SIF can be imported in Cytoscape.
 
 
@@ -6,6 +7,7 @@ library(igraph)
 
 # use absolute value
 cpm_pcc <- abs(cpm_pcc)
+diag(cpm_pcc) <- 0
 
 #choose threshold. For example, top 0.1%, 0.01%.
 threshold <- quantile(cpm_pcc,0.999)
@@ -15,7 +17,7 @@ g  <- graph.adjacency((cpm_pcc+t(cpm_pcc))/2,mode = "undirected",weighted=TRUE)
 df <- get.data.frame(g)
 head(df)
 df_pass1 <- subset(df,weight>threshold)
-write.table(df_pass1,file = paste0("cpm_pcc",threshold,".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
+write.table(df_pass1,file = paste0("cpm_pcc_",round(threshold,3),".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
 proc.time() - ptm
 
 
