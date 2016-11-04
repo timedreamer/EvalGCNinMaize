3e6c84d4ff528f24c81d1b3a6dcc3bfa6198a965
JHuang <hjraikkonen@gmail.com>
2016-11-04 16:15:32 -0400

    add aggregate newtwork without ranking norm

--- src/auc_cpm_evalAgg_total.R
+++ src/auc_cpm_evalAgg_total.R
@@ -2,8 +2,9 @@
 # then calculate AUC for the "aggregated network" from Protein-protein
 # interaction dataset as well as Gene Ontology.
 
-
-
+############################################################################################################
+####PART I NORMALIZED BY RANKING
+########################################################################################################
 # use absolute value for PCC, then convert PCC to ranking(same method used in EGAD)
 cpm12_pcc <- abs(cpm12_pcc);cpm12_pcc <- cpm12_pcc/max(cpm12_pcc,na.rm=T);diag(cpm12_pcc) <- 1
 cpm36_pcc <- abs(cpm36_pcc);cpm36_pcc <- cpm36_pcc/max(cpm36_pcc,na.rm=T);diag(cpm36_pcc) <- 1
@@ -60,7 +61,7 @@ cpm108_clr <- cpm108_clr/max(cpm108_clr,na.rm=T);diag(cpm108_clr) <- 1
 cpm270_clr <- cpm270_clr/max(cpm270_clr,na.rm=T);diag(cpm270_clr) <- 1
 cpm404_clr <- cpm404_clr/max(cpm404_clr,na.rm=T);diag(cpm404_clr) <- 1
 
-agg_ntwk <- abs(cpm12_clr) + abs(cpm36_clr) + abs(cpm65_clr) + abs(cpm108_clr) + abs(cpm270_clr) + abs(cpm404_clr)
+agg_ntwk <- cpm12_clr + cpm36_clr + cpm65_clr + cpm108_clr + cpm270_clr + cpm404_clr
 
 cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)] #                      #
 auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
@@ -71,3 +72,52 @@ save(auc_agg_test,file="agg_cpm_clr_PPPTY.RData")
 GO_clr_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub)
 GO_clr_agg_ntwk[[3]]
 save(GO_clr_agg_ntwk,file="agg_cpm_clr_GO.RData")
+
+
+
+######################################################################################################
+#### WITHOUT RANKING
+#####################################################################################################
+
+#PCC
+agg_ntwk <- abs(cpm12_pcc) + abs(cpm36_pcc) + abs(cpm65_pcc) + abs(cpm108_pcc) + abs(cpm270_pcc) + abs(cpm404_pcc)
+
+cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
+mean(unlist(auc_agg_test)) #
+auc_agg_pcc_noRank <- auc_agg_test
+save(auc_agg_pcc_noRank,file="agg_cpm_pcc_PPPTY_noRank.RData")
+
+GO_pcc_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub)
+GO_pcc_agg_ntwk[[3]]
+GO_agg_pcc_noRank <- GO_pcc_agg_ntwk
+save(GO_agg_pcc_noRank,file="agg_cpm_pcc_GO_noRank.RData")
+
+
+#MRNET
+agg_ntwk <- cpm12_mrnet + cpm36_mrnet + cpm65_mrnet + cpm108_mrnet + cpm270_mrnet + cpm404_mrnet
+
+cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
+mean(unlist(auc_agg_test)) #
+auc_agg_mrnet_noRank <- auc_agg_test
+save(auc_agg_mrnet_noRank,file="agg_cpm_mrnet_PPPTY_noRank.RData")
+
+GO_mrnet_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub)
+GO_mrnet_agg_ntwk[[3]]
+GO_agg_mrnet_noRank <- GO_mrnet_agg_ntwk
+save(GO_agg_mrnet_noRank,file="agg_cpm_mrnet_GO_noRank.RData")
+
+#CLR
+agg_ntwk <- cpm12_clr + cpm36_clr + cpm65_clr + cpm108_clr + cpm270_clr + cpm404_clr
+
+cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
+mean(unlist(auc_agg_test)) #
+auc_agg_clr_noRank <- auc_agg_test
+save(auc_agg_clr_noRank,file="agg_cpm_clr_PPPTY_noRank.RData")
+
+GO_clr_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub)
+GO_clr_agg_ntwk[[3]]
+GO_agg_clr_noRank <- GO_clr_agg_ntwk
+save(GO_agg_clr_noRank,file="agg_cpm_clr_GO_noRank.RData")
