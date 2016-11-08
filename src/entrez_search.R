35fce78a82a6fe397d331034d911ea3fe702e458
JHuang <hjraikkonen@gmail.com>
2016-11-08 13:46:58 -0500

    improve plot for entrez_search.R

--- src/entrez_search.R
+++ src/entrez_search.R
@@ -32,7 +32,7 @@ search_year_sra <- function(year, term){
 
 
 year <- 2008:2015
-sra_maize <- sapply(year, search_year, term="(Zea mays[ORGN] OR maize[ORGN]) AND RNA-Seq[STRA] AND illumina[PLAT]",
+sra_maize <- sapply(year, search_year_sra, term="(Zea mays[ORGN] OR maize[ORGN]) AND RNA-Seq[STRA] AND illumina[PLAT]",
                  USE.NAMES=FALSE)
 
 
@@ -41,24 +41,22 @@ sra_maize <- sapply(year, search_year, term="(Zea mays[ORGN] OR maize[ORGN]) AND
 geo_maize <- c(177,26,95,108,60,34,69,14)
 sra_maize <- c(0,6,9,31,460,524,811,2389)
 
-par(mfrow=c(1,2))
 
+par(mar=c(6,6,3,1))
 plot(sra_maize,type="b",col="red",xaxt="n", main="Maize RNA-Seq vs Microarray Samples",
-     ylab="number of samples",xlab="year")
-lines(geo_maize,type="b",col="blue")
-axis(1, at=1:8, labels=c(2008:2015))
+     ylab="number of samples",xlab="year",lwd=2.5,cex.axis =1.5,cex.lab=1.5,cex.main=2)
+lines(geo_maize,type="b",col="blue",lwd=2.5)
+axis(1, at=1:8, labels=c(2008:2015),cex.axis=1.5,cex.lab=2)
 
 
 # Or use Excel to draw graph
 geo_maize;sra_maize
 
-
 geo_total <- 718 # plus GPL12620 is 900
-
 sra_total <- 5056
-
 c_total <- c(geo_total,sra_total)
 names(c_total) <- c("Microarray","RNA-Seq")
 
-barplot(c_total,col="black",main="Total maize RNA-Seq vs Microarray")
-
+par(mar=c(6,10,10,6))
+barplot(c_total,col="black",main="Total maize RNA-Seq vs Microarray",
+        ylab="number of samples",cex.main=2.5,cex.lab=1.5,cex.axis=1.5,cex.names = 2)
