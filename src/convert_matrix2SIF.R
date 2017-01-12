3b5d43aef636a27cc7fdfb44f04e49007f33c66c
timedreamer <timedreamer@users.noreply.github.com>
2017-01-12 11:09:21 -0500

    top1M from three agg ntwks

--- src/convert_matrix2SIF.R
+++ src/convert_matrix2SIF.R
@@ -2,36 +2,65 @@
 # For correlation, change diag to 0 to avoid self-connections.
 # The resulting SIF can be imported in Cytoscape.
 
-
+# KIN3077 Windows
 library(igraph)
+load("~/Co-expression/aggregate_ntwk/agg15_ntwkPCC.Rdata")
+load("~/Co-expression/aggregate_ntwk/agg15_ntwkMrnet.Rdata")
+load("~/Co-expression/aggregate_ntwk/agg15_ntwkClr.Rdata")
 
+# CPM_all_fifteen_maize
 # use absolute value
-cpm_pcc <- abs(cpm_pcc)
-diag(cpm_pcc) <- 0
+agg15_ntwkPcc <- abs(agg15_ntwkPcc)
+diag(agg15_ntwkPcc) <- 0
 
-#choose threshold. For example, top 0.1%, 0.01%.
-threshold <- quantile(cpm_pcc,0.999)
+#choose threshold.top 1 Million 
+one_million <- 1e06/(15116*15116)
+
+threshold <- quantile(agg15_ntwkPcc,1-one_million)
 
 ptm <- proc.time()
-g  <- graph.adjacency((cpm_pcc+t(cpm_pcc))/2,mode = "undirected",weighted=TRUE)
+g  <- graph.adjacency((agg15_ntwkPcc+t(agg15_ntwkPcc))/2,mode = "undirected",weighted=TRUE)
 df <- get.data.frame(g)
 head(df)
 df_pass1 <- subset(df,weight>threshold)
-write.table(df_pass1,file = paste0("cpm_pcc_",round(threshold,3),".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
-proc.time() - ptm # For threshold=0.99 took ~85s.
+write.table(df_pass1,file = paste0("agg15_ntwkPcc_",round(threshold,3),".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
+proc.time() - ptm # For threshold top 1million, 80.73s
 
 
-# CLR
-cpm_clr <- abs(cpm_clr)
-diag(cpm_clr) <- 0
+# MRNET_all_fifteen_maize
+# use absolute value
+agg15_ntwkMrnet <- abs(agg15_ntwkMrnet)
+diag(agg15_ntwkMrnet) <- 0
 
-#choose threshold. For example, top 0.1%, 0.01%.
-threshold <- quantile(cpm_clr,0.999)
+#choose threshold.top 1 Million 
+one_million <- 1e06/(15116*15116)
+
+threshold <- quantile(agg15_ntwkMrnet,1-one_million)
 
 ptm <- proc.time()
-g  <- graph.adjacency((cpm_clr+t(cpm_clr))/2,mode = "undirected",weighted=TRUE)
+g  <- graph.adjacency((agg15_ntwkMrnet+t(agg15_ntwkMrnet))/2,mode = "undirected",weighted=TRUE)
 df <- get.data.frame(g)
 head(df)
 df_pass1 <- subset(df,weight>threshold)
-write.table(df_pass1,file = paste0("cpm_clr_",round(threshold,3),".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
-proc.time() - ptm # 0.999 threshold 18.519
+write.table(df_pass1,file = paste0("agg15_ntwkMrnet_",round(threshold,3),".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
+proc.time() - ptm # For threshold top 1million, 80.45s
+
+
+
+# CLR_all_fifteen_maize
+# use absolute value
+agg15_ntwkClr <- abs(agg15_ntwkClr)
+diag(agg15_ntwkClr) <- 0
+
+#choose threshold.top 1 Million 
+one_million <- 1e06/(15116*15116)
+
+threshold <- quantile(agg15_ntwkClr,1-one_million)
+
+ptm <- proc.time()
+g  <- graph.adjacency((agg15_ntwkClr+t(agg15_ntwkClr))/2,mode = "undirected",weighted=TRUE)
+df <- get.data.frame(g)
+head(df)
+df_pass1 <- subset(df,weight>threshold)
+write.table(df_pass1,file = paste0("agg15_ntwkClr_",round(threshold,3),".SIF"),quote = F,sep = "\t",col.names = T,row.names = F)
+proc.time() - ptm # For threshold top 1million, 80.85s
