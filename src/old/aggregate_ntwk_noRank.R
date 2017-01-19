9952456bc365fdf0a09ac3b3a2b24cb1e8ffe684
JHuang <hjraikkonen@gmail.com>
2017-01-19 15:12:01 -0500

    old files, nouse

---
+++ src/old/aggregate_ntwk_noRank.R
@@ -0,0 +1,116 @@
+# This script is to calculate the AUC, both PPPTY and GO for the aggregation of all 15 experiments
+# within the 1266 libs. Testeds.
+# PS: exp14_cpm_pcc need to change NA to 0 before aggregation.
+
+
+######################################################################################################
+#### WITHOUT RANKING STEP
+#####################################################################################################
+
+setwd("D:\\Users\\jhuang\\Documents\\Co-expression\\aggregate_ntwk")
+
+###PCC
+
+# load data
+load("./12/12_cpm_pcc.RData");load("./36/36_cpm_pcc.RData");load("./65/65_cpm_pcc.RData")
+load("./108/108_cpm_pcc.RData");load("270/270_cpm_pcc.RData");load("404/404_cpm_pcc.RData")
+load("Exp7/exp7_cpm_pcc.RData");load("Exp8/exp8_cpm_pcc.RData");load("Exp8/exp8_cpm_pcc.RData")
+load("Exp9/exp9_cpm_pcc.RData");load("Exp10/exp10_cpm_pcc.RData");load("Exp11/exp11_cpm_pcc.RData")
+load("Exp12/exp12_cpm_pcc.RData");load("Exp13/exp13_cpm_pcc.RData");load("Exp13/exp13_cpm_pcc.RData")
+load("Exp15/exp15_cpm_pcc.RData");
+load("Exp14/exp14_cpm_pcc.RData"); # somehow the exp14_pcc has NA in the matrix.
+
+cpmexp14_pcc[is.na(cpmexp14_pcc)] <- 0 # need to change NA to 0.
+
+agg_ntwk <- abs(cpm12_pcc) + abs(cpm36_pcc) + abs(cpm65_pcc) + abs(cpm108_pcc) +
+  abs(cpm270_pcc) + abs(cpm404_pcc) + abs(cpmexp7_pcc) +abs(cpmexp8_pcc) +
+  abs(cpmexp9_pcc) +abs(cpmexp10_pcc) +abs(cpmexp11_pcc) +abs(cpmexp12_pcc) +
+  abs(cpmexp13_pcc) +abs(cpmexp15_pcc) + abs(cpmexp14_pcc)
+
+rm(cpm12_pcc,cpm36_pcc,cpm65_pcc,cpm108_pcc,cpm270_pcc,cpm404_pcc,cpmexp7_pcc,cpmexp8_pcc,
+   cpmexp9_pcc,cpmexp10_pcc,cpmexp11_pcc,cpmexp12_pcc,cpmexp13_pcc,cpmexp14_pcc,cpmexp15_pcc)
+
+agg15_ntwkPcc <- agg_ntwk
+save(agg15_ntwkPcc,file="agg15_ntwkPCC.Rdata") # save the aggregated network PCC
+
+cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)]                      #
+auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
+mean(unlist(auc_agg_test)) #
+auc_agg_pcc_noRank <- auc_agg_test
+save(auc_agg_pcc_noRank,file="agg_cpm_pccAll_PPPTY_noRank.RData")
+
+
+system.time(GO_pcc_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub))
+GO_pcc_agg_ntwk[[3]]
+save(GO_pcc_agg_ntwk,file="agg_cpmPccAll_GO_noRank.RData")
+
+rm(GO_pcc_agg_ntwk,agg_ntwk,agg_ntwk)
+
+
+###MRNET
+
+# load data
+load("./12/12_cpm_mrnet.RData");load("./36/36_cpm_mrnet.RData");load("./65/65_cpm_mrnet.RData")
+load("./108/108_cpm_mrnet.RData");load("270/270_cpm_mrnet.RData");load("404/404_cpm_mrnet.RData")
+load("Exp7/exp7_cpm_mrnet.RData");load("Exp8/exp8_cpm_mrnet.RData");load("Exp8/exp8_cpm_mrnet.RData")
+load("Exp9/exp9_cpm_mrnet.RData");load("Exp10/exp10_cpm_mrnet.RData");load("Exp11/exp11_cpm_mrnet.RData")
+load("Exp12/exp12_cpm_mrnet.RData");load("Exp13/exp13_cpm_mrnet.RData");load("Exp14/exp14_cpm_mrnet.RData")
+load("Exp15/exp15_cpm_mrnet.RData")
+
+agg_ntwk <- abs(cpm12_mrnet) + abs(cpm36_mrnet) + abs(cpm65_mrnet) + abs(cpm108_mrnet) +
+  abs(cpm270_mrnet) + abs(cpm404_mrnet) + abs(cpmexp7_mrnet) +abs(cpmexp8_mrnet) +
+  abs(cpmexp9_mrnet) +abs(cpmexp10_mrnet) +abs(cpmexp11_mrnet) +abs(cpmexp12_mrnet) +
+  abs(cpmexp13_mrnet) +abs(cpmexp15_mrnet) + abs(cpmexp14_mrnet)
+
+
+rm(cpm12_mrnet,cpm36_mrnet,cpm65_mrnet,cpm108_mrnet,cpm270_mrnet,cpm404_mrnet,cpmexp7_mrnet,cpmexp8_mrnet,
+   cpmexp9_mrnet,cpmexp10_mrnet,cpmexp11_mrnet,cpmexp12_mrnet,cpmexp13_mrnet,cpmexp14_mrnet,cpmexp15_mrnet)
+
+agg15_ntwkMrnet <- agg_ntwk
+save(agg15_ntwkMrnet,file="agg15_ntwkMrnet.Rdata") # save the aggregated network Mrnet
+
+cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
+mean(unlist(auc_agg_test)) #
+auc_agg_mrnet_noRank <- auc_agg_test
+save(auc_agg_mrnet_noRank,file="agg_cpm_mrnetAll_PPPTY_noRank.RData")
+
+GO_mrnet_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub)
+GO_mrnet_agg_ntwk[[3]]
+GO_agg_mrnet_noRank <- GO_mrnet_agg_ntwk
+save(GO_agg_mrnet_noRank,file="agg_cpmMrnetAll_GO_noRank.RData")
+
+
+###CLR
+
+# load data
+load("./12/12_cpm_clr.RData");load("./36/36_cpm_clr.RData");load("./65/65_cpm_clr.RData")
+load("./108/108_cpm_clr.RData");load("270/270_cpm_clr.RData");load("404/404_cpm_clr.RData")
+load("Exp7/exp7_cpm_clr.RData");load("Exp8/exp8_cpm_clr.RData");load("Exp8/exp8_cpm_clr.RData")
+load("Exp9/exp9_cpm_clr.RData");load("Exp10/exp10_cpm_clr.RData");load("Exp11/exp11_cpm_clr.RData")
+load("Exp12/exp12_cpm_clr.RData");load("Exp13/exp13_cpm_clr.RData");load("Exp14/exp14_cpm_clr.RData")
+load("Exp15/exp15_cpm_clr.RData")
+
+
+agg_ntwk <- abs(cpm12_clr) + abs(cpm36_clr) + abs(cpm65_clr) + abs(cpm108_clr) +
+  abs(cpm270_clr) + abs(cpm404_clr) + abs(cpmexp7_clr) +abs(cpmexp8_clr) +
+  abs(cpmexp9_clr) +abs(cpmexp10_clr) +abs(cpmexp11_clr) +abs(cpmexp12_clr) +
+  abs(cpmexp13_clr) +abs(cpmexp15_clr) + abs(cpmexp14_clr)
+
+
+rm(cpm12_clr,cpm36_clr,cpm65_clr,cpm108_clr,cpm270_clr,cpm404_clr,cpmexp7_clr,cpmexp8_clr,
+   cpmexp9_clr,cpmexp10_clr,cpmexp11_clr,cpmexp12_clr,cpmexp13_clr,cpmexp15_clr,cpmexp14_clr)
+
+agg15_ntwkClr <- agg_ntwk
+save(agg15_ntwkClr,file="agg15_ntwkClr.Rdata") # save the aggregated network Clr
+
+cpmagg_test_ft <- agg_ntwk[,which(colnames(agg_ntwk) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_agg_test <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=cpmagg_test_ft)                   #
+mean(unlist(auc_agg_test)) #
+auc_agg_clr_noRank <- auc_agg_test
+save(auc_agg_clr_noRank,file="agg_cpm_clrAll_PPPTY_noRank.RData")
+
+GO_clr_agg_ntwk <- run_GBA(agg_ntwk,annotations_sub)
+GO_clr_agg_ntwk[[3]]
+GO_agg_clr_noRank <- GO_clr_agg_ntwk
+save(GO_agg_clr_noRank,file="agg_cpmClrAll_GO_noRank.RData")
---
+++ src/old/arab_aggregate_ntwk.R
@@ -0,0 +1,232 @@
+# this file is to calculat arabidopsis aggrgated networks.
+library(EGAD)
+
+# pcc
+setwd("D:/Users/jhuang/Documents/Co-expression/arabidopsis/Text_arab/arab_all_pcc")
+
+load("exp01_cpm_pcc.RData");load("exp02_cpm_pcc.RData");load("exp03_cpm_pcc.RData");load("exp04_cpm_pcc.RData");load("exp05_cpm_pcc.RData");
+load("exp06_cpm_pcc.RData");load("exp07_cpm_pcc.RData");load("exp08_cpm_pcc.RData");load("exp09_cpm_pcc.RData");load("exp10_cpm_pcc.RData");
+load("exp11_cpm_pcc.RData");load("exp12_cpm_pcc.RData");load("exp13_cpm_pcc.RData");load("exp14_cpm_pcc.RData");load("exp15_cpm_pcc.RData");
+load("exp16_cpm_pcc.RData");load("exp17_cpm_pcc.RData");load("exp18_cpm_pcc.RData");load("exp19_cpm_pcc.RData");load("exp20_cpm_pcc.RData");
+load("exp21_cpm_pcc.RData");load("exp22_cpm_pcc.RData");load("exp23_cpm_pcc.RData");load("exp24_cpm_pcc.RData");load("exp25_cpm_pcc.RData");
+load("exp26_cpm_pcc.RData");load("exp27_cpm_pcc.RData");load("exp28_cpm_pcc.RData");load("exp29_cpm_pcc.RData");load("exp30_cpm_pcc.RData");
+load("exp31_cpm_pcc.RData");load("exp32_cpm_pcc.RData");load("exp33_cpm_pcc.RData");load("exp34_cpm_pcc.RData");load("exp35_cpm_pcc.RData");
+load("exp36_cpm_pcc.RData");load("exp37_cpm_pcc.RData");load("exp38_cpm_pcc.RData");load("exp39_cpm_pcc.RData");load("exp40_cpm_pcc.RData");
+load("exp41_cpm_pcc.RData");load("exp42_cpm_pcc.RData");load("exp43_cpm_pcc.RData")
+
+cpmexp01_pcc[is.na(cpmexp01_pcc)] <- 0;cpmexp02_pcc[is.na(cpmexp02_pcc)] <- 0;cpmexp03_pcc[is.na(cpmexp03_pcc)] <- 0
+cpmexp04_pcc[is.na(cpmexp04_pcc)] <- 0;cpmexp05_pcc[is.na(cpmexp05_pcc)] <- 0;cpmexp06_pcc[is.na(cpmexp06_pcc)] <- 0
+cpmexp07_pcc[is.na(cpmexp07_pcc)] <- 0;cpmexp08_pcc[is.na(cpmexp08_pcc)] <- 0;cpmexp09_pcc[is.na(cpmexp09_pcc)] <- 0;cpmexp10_pcc[is.na(cpmexp10_pcc)] <- 0
+cpmexp11_pcc[is.na(cpmexp11_pcc)] <- 0;cpmexp12_pcc[is.na(cpmexp12_pcc)] <- 0;cpmexp13_pcc[is.na(cpmexp13_pcc)] <- 0;cpmexp14_pcc[is.na(cpmexp14_pcc)] <- 0
+cpmexp15_pcc[is.na(cpmexp15_pcc)] <- 0;cpmexp16_pcc[is.na(cpmexp16_pcc)] <- 0;cpmexp17_pcc[is.na(cpmexp17_pcc)] <- 0;cpmexp18_pcc[is.na(cpmexp18_pcc)] <- 0
+cpmexp19_pcc[is.na(cpmexp19_pcc)] <- 0;cpmexp20_pcc[is.na(cpmexp20_pcc)] <- 0;cpmexp21_pcc[is.na(cpmexp21_pcc)] <- 0;cpmexp22_pcc[is.na(cpmexp22_pcc)] <- 0
+cpmexp23_pcc[is.na(cpmexp23_pcc)] <- 0;cpmexp24_pcc[is.na(cpmexp24_pcc)] <- 0;cpmexp25_pcc[is.na(cpmexp25_pcc)] <- 0;cpmexp26_pcc[is.na(cpmexp26_pcc)] <- 0
+cpmexp27_pcc[is.na(cpmexp27_pcc)] <- 0;cpmexp28_pcc[is.na(cpmexp28_pcc)] <- 0;cpmexp29_pcc[is.na(cpmexp29_pcc)] <- 0;cpmexp30_pcc[is.na(cpmexp30_pcc)] <- 0
+cpmexp31_pcc[is.na(cpmexp31_pcc)] <- 0;cpmexp32_pcc[is.na(cpmexp32_pcc)] <- 0;cpmexp33_pcc[is.na(cpmexp33_pcc)] <- 0;cpmexp34_pcc[is.na(cpmexp34_pcc)] <- 0
+cpmexp35_pcc[is.na(cpmexp35_pcc)] <- 0;cpmexp36_pcc[is.na(cpmexp36_pcc)] <- 0;cpmexp37_pcc[is.na(cpmexp37_pcc)] <- 0;cpmexp38_pcc[is.na(cpmexp38_pcc)] <- 0
+cpmexp39_pcc[is.na(cpmexp39_pcc)] <- 0;cpmexp40_pcc[is.na(cpmexp40_pcc)] <- 0;cpmexp41_pcc[is.na(cpmexp41_pcc)] <- 0;cpmexp42_pcc[is.na(cpmexp42_pcc)] <- 0
+cpmexp43_pcc[is.na(cpmexp43_pcc)] <- 0
+
+agg_ntwk_pcc <- abs(cpmexp01_pcc) + abs(cpmexp02_pcc) +abs(cpmexp03_pcc) +abs(cpmexp04_pcc) +abs(cpmexp05_pcc) +abs(cpmexp06_pcc) +abs(cpmexp07_pcc) +
+  abs(cpmexp08_pcc) +abs(cpmexp09_pcc) +abs(cpmexp10_pcc) +abs(cpmexp11_pcc) +abs(cpmexp12_pcc) +abs(cpmexp13_pcc) +abs(cpmexp14_pcc) +
+  abs(cpmexp15_pcc) +abs(cpmexp16_pcc) +abs(cpmexp17_pcc) +abs(cpmexp18_pcc) +abs(cpmexp19_pcc) +abs(cpmexp20_pcc) +abs(cpmexp21_pcc) +abs(cpmexp22_pcc) +
+  abs(cpmexp23_pcc) +abs(cpmexp24_pcc) +abs(cpmexp25_pcc) +abs(cpmexp26_pcc) +abs(cpmexp27_pcc) +abs(cpmexp28_pcc) +abs(cpmexp29_pcc) +
+  abs(cpmexp30_pcc) +abs(cpmexp31_pcc) +abs(cpmexp32_pcc) +abs(cpmexp33_pcc) +abs(cpmexp34_pcc) +abs(cpmexp35_pcc) +abs(cpmexp36_pcc) +
+  abs(cpmexp37_pcc) +abs(cpmexp38_pcc) +abs(cpmexp39_pcc) +abs(cpmexp40_pcc) +abs(cpmexp41_pcc) +abs(cpmexp42_pcc) +abs(cpmexp43_pcc)
+
+
+save(agg_ntwk_pcc,file="arab_agg_ntwk_pcc.Rdata") # save the aggregated network pcc
+rm(list = ls(pattern = "*_pcc"))
+
+load("~/Co-expression/arabidopsis/Text_arab/arab_all_pcc/arab_agg_ntwk_pcc.Rdata")
+GO_arab_agg_pcc <- run_GBA(agg_ntwk_pcc,annotations_sub)
+GO_arab_agg_pcc[[3]]
+save(GO_arab_agg_pcc,file="GO_arab_agg_pcc.RData")
+
+#indi 
+load("~/Co-expression/arabidopsis/arabidopsis_GO_pcc.RData")
+GO_arab_pcc[[3]]
+
+pcc_indi <- GO_arab_pcc[[1]][,1]
+pcc_agg <- GO_arab_agg_pcc[[1]][,1]
+wilcox.test(pcc_indi,pcc_agg)
+t1 <- pcc_indi - pcc_agg
+table(t1>0)
+
+
+# mrnet
+setwd("D:/Users/jhuang/Documents/Co-expression/arabidopsis/Text_arab/arab_all_mrnet")
+
+load("exp01_cpm_mrnet.RData");load("exp02_cpm_mrnet.RData");load("exp03_cpm_mrnet.RData");load("exp04_cpm_mrnet.RData");load("exp05_cpm_mrnet.RData");
+load("exp06_cpm_mrnet.RData");load("exp07_cpm_mrnet.RData");load("exp08_cpm_mrnet.RData");load("exp09_cpm_mrnet.RData");load("exp10_cpm_mrnet.RData");
+load("exp11_cpm_mrnet.RData");load("exp12_cpm_mrnet.RData");load("exp13_cpm_mrnet.RData");load("exp14_cpm_mrnet.RData");load("exp15_cpm_mrnet.RData");
+load("exp16_cpm_mrnet.RData");load("exp17_cpm_mrnet.RData");load("exp18_cpm_mrnet.RData");load("exp19_cpm_mrnet.RData");load("exp20_cpm_mrnet.RData");
+load("exp21_cpm_mrnet.RData");load("exp22_cpm_mrnet.RData");load("exp23_cpm_mrnet.RData");load("exp24_cpm_mrnet.RData");load("exp25_cpm_mrnet.RData");
+load("exp26_cpm_mrnet.RData");load("exp27_cpm_mrnet.RData");load("exp28_cpm_mrnet.RData");load("exp29_cpm_mrnet.RData");load("exp30_cpm_mrnet.RData");
+load("exp31_cpm_mrnet.RData");load("exp32_cpm_mrnet.RData");load("exp33_cpm_mrnet.RData");load("exp34_cpm_mrnet.RData");load("exp35_cpm_mrnet.RData");
+load("exp36_cpm_mrnet.RData");load("exp37_cpm_mrnet.RData");load("exp38_cpm_mrnet.RData");load("exp39_cpm_mrnet.RData");load("exp40_cpm_mrnet.RData");
+load("exp41_cpm_mrnet.RData");load("exp42_cpm_mrnet.RData");load("exp43_cpm_mrnet.RData")
+
+
+agg_ntwk_mrnet <- abs(cpmexp01_mrnet) + abs(cpmexp02_mrnet) +abs(cpmexp03_mrnet) +abs(cpmexp04_mrnet) +abs(cpmexp05_mrnet) +abs(cpmexp06_mrnet) +abs(cpmexp07_mrnet) +
+  abs(cpmexp08_mrnet) +abs(cpmexp09_mrnet) +abs(cpmexp10_mrnet) +abs(cpmexp11_mrnet) +abs(cpmexp12_mrnet) +abs(cpmexp13_mrnet) +abs(cpmexp14_mrnet) +
+  abs(cpmexp15_mrnet) +abs(cpmexp16_mrnet) +abs(cpmexp17_mrnet) +abs(cpmexp18_mrnet) +abs(cpmexp19_mrnet) +abs(cpmexp20_mrnet) +abs(cpmexp21_mrnet) +abs(cpmexp22_mrnet) +
+  abs(cpmexp23_mrnet) +abs(cpmexp24_mrnet) +abs(cpmexp25_mrnet) +abs(cpmexp26_mrnet) +abs(cpmexp27_mrnet) +abs(cpmexp28_mrnet) +abs(cpmexp29_mrnet) +
+  abs(cpmexp30_mrnet) +abs(cpmexp31_mrnet) +abs(cpmexp32_mrnet) +abs(cpmexp33_mrnet) +abs(cpmexp34_mrnet) +abs(cpmexp35_mrnet) +abs(cpmexp36_mrnet) +
+  abs(cpmexp37_mrnet) +abs(cpmexp38_mrnet) +abs(cpmexp39_mrnet) +abs(cpmexp40_mrnet) +abs(cpmexp41_mrnet) +abs(cpmexp42_mrnet) +abs(cpmexp43_mrnet)
+
+
+
+save(agg_ntwk_mrnet,file="arab_agg_ntwk_mrnet.Rdata") # save the aggregated network mrnet
+
+rm(list = ls(pattern = "*_mrnet"))
+
+load("~/Co-expression/arabidopsis/Text_arab/arab_all_mrnet/arab_agg_ntwk_mrnet.Rdata")
+GO_arab_agg_mrnet <- run_GBA(agg_ntwk_mrnet,annotations_sub)
+
+save(GO_arab_agg_mrnet,file="GO_arab_agg_mrnet.RData")
+
+GO_arab_agg_pcc[[3]]
+GO_arab_agg_mrnet[[3]]
+GO_arab_agg_clr[[3]]
+
+GO_arab_pcc[[3]]
+GO_arab_mrnet[[3]]
+GO_arab_clr[[3]]
+
+wilcox.test(GO_arab_agg_pcc[[1]][,1], GO_arab_agg_clr[[1]][,1])
+
+# CLR
+setwd("D:/Users/jhuang/Documents/Co-expression/arabidopsis/Text_arab/arab_all_clr")
+
+load("exp01_cpm_clr.RData");load("exp02_cpm_clr.RData");load("exp03_cpm_clr.RData");load("exp04_cpm_clr.RData");load("exp05_cpm_clr.RData");
+load("exp06_cpm_clr.RData");load("exp07_cpm_clr.RData");load("exp08_cpm_clr.RData");load("exp09_cpm_clr.RData");load("exp10_cpm_clr.RData");
+load("exp11_cpm_clr.RData");load("exp12_cpm_clr.RData");load("exp13_cpm_clr.RData");load("exp14_cpm_clr.RData");load("exp15_cpm_clr.RData");
+load("exp16_cpm_clr.RData");load("exp17_cpm_clr.RData");load("exp18_cpm_clr.RData");load("exp19_cpm_clr.RData");load("exp20_cpm_clr.RData");
+load("exp21_cpm_clr.RData");load("exp22_cpm_clr.RData");load("exp23_cpm_clr.RData");load("exp24_cpm_clr.RData");load("exp25_cpm_clr.RData");
+load("exp26_cpm_clr.RData");load("exp27_cpm_clr.RData");load("exp28_cpm_clr.RData");load("exp29_cpm_clr.RData");load("exp30_cpm_clr.RData");
+load("exp31_cpm_clr.RData");load("exp32_cpm_clr.RData");load("exp33_cpm_clr.RData");load("exp34_cpm_clr.RData");load("exp35_cpm_clr.RData");
+load("exp36_cpm_clr.RData");load("exp37_cpm_clr.RData");load("exp38_cpm_clr.RData");load("exp39_cpm_clr.RData");load("exp40_cpm_clr.RData");
+load("exp41_cpm_clr.RData");load("exp42_cpm_clr.RData");load("exp43_cpm_clr.RData")
+
+
+agg_ntwk_clr <- abs(cpmexp01_clr) + abs(cpmexp02_clr) +abs(cpmexp03_clr) +abs(cpmexp04_clr) +abs(cpmexp05_clr) +abs(cpmexp06_clr) +abs(cpmexp07_clr) +
+  abs(cpmexp08_clr) +abs(cpmexp09_clr) +abs(cpmexp10_clr) +abs(cpmexp11_clr) +abs(cpmexp12_clr) +abs(cpmexp13_clr) +abs(cpmexp14_clr) +
+  abs(cpmexp15_clr) +abs(cpmexp16_clr) +abs(cpmexp17_clr) +abs(cpmexp18_clr) +abs(cpmexp19_clr) +abs(cpmexp20_clr) +abs(cpmexp21_clr) +abs(cpmexp22_clr) +
+  abs(cpmexp23_clr) +abs(cpmexp24_clr) +abs(cpmexp25_clr) +abs(cpmexp26_clr) +abs(cpmexp27_clr) +abs(cpmexp28_clr) +abs(cpmexp29_clr) +
+  abs(cpmexp30_clr) +abs(cpmexp31_clr) +abs(cpmexp32_clr) +abs(cpmexp33_clr) +abs(cpmexp34_clr) +abs(cpmexp35_clr) +abs(cpmexp36_clr) +
+  abs(cpmexp37_clr) +abs(cpmexp38_clr) +abs(cpmexp39_clr) +abs(cpmexp40_clr) +abs(cpmexp41_clr) +abs(cpmexp42_clr) +abs(cpmexp43_clr)
+
+
+
+save(agg_ntwk_clr,file="arab_agg_ntwk_clr.Rdata") # save the aggregated network Clr
+
+
+rm(list = ls(pattern = "*_clr"))
+GO_arab_agg_clr <- run_GBA(agg_ntwk_clr,annotations_sub)
+
+save(GO_arab_agg_clr,file="GO_arab_agg_clr.RData")
+
+
+
+
+###Protein-protein 
+
+library(ROCR)
+calc_auc <- function(corMatrix,x){
+  t1 <- abs(corMatrix[,x])
+  m1 <- data.frame(matrix(nrow = 13519,ncol=3))
+  m1[,1] <- as.character();m1[,2] <- as.double();m1[,3] <- 0
+  m1[,1] <- names(t1);m1[,2] <- t1;m1[which(m1[,1] %in% cogene_result[[x]]),3] <- 1
+  if (length(table(m1[,3]))==2){
+    pred <- prediction(m1[,2],m1[,3])
+    auc <- performance(pred,"auc");auc <- unlist(slot(auc, "y.values"))
+  } else {
+    auc <- NULL
+  }
+  return(auc)
+}
+
+filter_node_name <- node_name[which(node_name %in% colnames(agg_ntwk_pcc))]
+cogene_result <- cogene_result[names(cogene_result) %in% filter_node_name]
+
+ara_pcc_new <- (agg_ntwk_pcc - mean(agg_ntwk_pcc))/sd(agg_ntwk_pcc) 
+
+# the pcc correlaition matrix was right shifted a little.
+# this cause AUROC higher, like what happend for RC.So I did a normalization.
+ara_pcc_ft <- ara_pcc_new[,which(colnames(ara_pcc_new) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_ara_pcc <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=ara_pcc_new);
+mean(unlist(auc_ara_pcc));
+
+ara_mrnet_ft <- agg_ntwk_mrnet[,which(colnames(agg_ntwk_mrnet) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_ara_mrnet <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=agg_ntwk_mrnet);
+mean(unlist(auc_ara_mrnet));
+
+ara_clr_ft <- agg_ntwk_clr[,which(colnames(agg_ntwk_clr) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_ara_clr <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=agg_ntwk_clr);
+mean(unlist(auc_ara_clr));
+
+save(auc_ara_pcc,auc_ara_mrnet,auc_ara_clr,file="auc_AggPP_3Methods.RData")
+
+ap <- unlist(auc_ara_pcc);am <- unlist(auc_ara_mrnet);ac <- unlist(auc_ara_clr)
+
+agg_ap <- unlist(auc_ara_pcc);agg_am <- unlist(auc_ara_mrnet);agg_ac <- unlist(auc_ara_clr)
+mean(ac);mean(agg_ac)
+wilcox.test(ac,agg_ac,paired = F,alternative = "g")
+
+agg <- cbind(agg_ap,agg_am,agg_ac);agg_name <- c(rep("pcc",3138),rep("mrnet",3138),rep("clr",3138))
+pairwise.wilcox.test(agg,agg_name,p.adjust.method = 'b')
+
+total_1363 <- cbind(ap,am,ac);total_1363Name <- c(rep("pcc",3138),rep("mrnet",3138),rep("clr",3138))
+pairwise.wilcox.test(total_1363,total_1363Name,p.adjust.method = 'b')
+
+
+################calculation for Ranked arabidopsis aggregation network
+load("~/Co-expression/arabidopsis/agg_rank/arab_agg_ntwkRank_pcc.Rdata")
+load("~/Co-expression/arabidopsis/agg_rank/arab_agg_ntwkRank_mrnet.Rdata")
+load("~/Co-expression/arabidopsis/agg_rank/arab_agg_ntwkRank_clr.Rdata")
+
+GO_arab_aggRank_pcc <- run_GBA(agg_ntwk_pccRank,annotations_sub)
+GO_arab_aggRank_pcc[[3]]
+save(GO_arab_aggRank_pcc,file="GO_arab_aggRank_pcc.RData")
+
+GO_arab_aggRank_mrnet <- run_GBA(agg_ntwk_mrnetRank,annotations_sub)
+save(GO_arab_aggRank_mrnet,file="GO_arab_aggRank_mrnet.RData")
+
+GO_arab_aggRank_clr <- run_GBA(agg_ntwk_clrRank,annotations_sub)
+save(GO_arab_aggRank_clr,file="GO_arab_aggRank_clr.RData")
+
+p <- GO_arab_aggRank_pcc[[1]][,1];m <- GO_arab_aggRank_mrnet[[1]][,1]
+c <- GO_arab_aggRank_clr[[1]][,1]
+arabRank_total <- c(p,m,c)
+arabRank_total_name <- c(rep('pcc',445),rep('mrnet',445),rep('clr',445))
+rm(p,m,c)
+pairwise.wilcox.test(arabRank_total,arabRank_total_name,p.adjust.method = 'b',correct=F)
+
+## PP
+
+filter_node_name <- node_name[which(node_name %in% colnames(agg_ntwk_pccRank))]
+cogene_result <- cogene_result[names(cogene_result) %in% filter_node_name]
+
+ara_pcc_new <- (agg_ntwk_pccRank - mean(agg_ntwk_pccRank))/sd(agg_ntwk_pccRank) 
+
+# the pcc correlaition matrix was right shifted a little.
+# this cause AUROC higher, like what happend for RC.So I did a normalization.
+ara_pcc_ft <- ara_pcc_new[,which(colnames(ara_pcc_new) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_ara_pcc <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=ara_pcc_new);
+mean(unlist(auc_ara_pcc))
+auc_ara_pccRank <- auc_ara_pcc
+
+ara_mrnet_ft <- agg_ntwk_mrnetRank[,which(colnames(agg_ntwk_mrnetRank) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_ara_mrnet <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=agg_ntwk_mrnetRank);
+mean(unlist(auc_ara_mrnet));
+auc_ara_mrnetRank <- auc_ara_mrnet
+
+ara_clr_ft <- agg_ntwk_clrRank[,which(colnames(agg_ntwk_clrRank) %in% filter_node_name)] # dim vst_gcc_ft 15116*753                        #
+auc_ara_clr <- sapply(X = filter_node_name,FUN = calc_auc,corMatrix=agg_ntwk_clrRank);
+mean(unlist(auc_ara_clr))
+auc_ara_clrRank <- auc_ara_clr
+
+save(auc_ara_pccRank,auc_ara_mrnetRank,auc_ara_clrRank,file="arab_PPPTY_aggRank.RData")
+
+
