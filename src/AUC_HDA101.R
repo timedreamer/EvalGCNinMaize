55d0a09b5480d5e84095c50eb8a6bf98ed1bd587
JHuang <hjraikkonen@gmail.com>
2017-02-10 14:09:58 -0500

    use rank hda101 data

--- src/AUC_HDA101.R
+++ src/AUC_HDA101.R
@@ -11,12 +11,11 @@ setwd("C:\\WORK\\Co-expression\\ChIPSeq\\HDA101")
 upGenes <- read.delim("HDA101_increasedExpressGenes.txt",header=T,sep="\t")
 name3 <- upGenes$Gene
 
-setwd("C:\\WORK\\Co-expression\\ChIPSeq\\HDA101\\correlationCoefficient")
 
 
 
-hda101_vst_gcc <- read.delim("GRMZM2G172883_HDA101.rcclr",header=F,sep='\t')
-hda101_vst_gcc[,2] <- abs(hda101_vst_gcc[,2])
+setwd("C:\\WORK\\EvalGCNinMaize\\data\\hda101_rank")
+hda101_vst_gcc <- read.delim("hda101.vstclr",header=F,sep='\t')
 # result without sorting is the same as sorting.
 
 rownames(hda101_vst_gcc) <- hda101_vst_gcc[,1]
@@ -39,17 +38,15 @@ colnames(hda101_vst_gcc) <- c("Gene","Score","Target")
 
 pred <- prediction(hda101_vst_gcc$Score,hda101_vst_gcc$Target)
 perf <- performance(pred, measure = "tpr", x.measure = "fpr")
-
-plot(perf,main="hda101_rpkmClr")
-abline(0,1,col="red")
-table(hda101_vst_gcc$Target)
-
-
+# plot(perf,main="hda101_rpkmClr")
+# abline(0,1,col="red")
+# table(hda101_vst_gcc$Target)
 auc <- performance(pred,"auc")
 auc <- unlist(slot(auc, "y.values"))
 auc <- round(auc, digits = 3)
-auct <- paste(c("AUC = "),auc,sep="")
-legend(0.65,0.3,auct,border="white",cex=1.4,box.col = "white")
+1-auc
+# auct <- paste(c("AUC = "),auc,sep="")
+# legend(0.65,0.3,auct,border="white",cex=1.4,box.col = "white")
 
 
 ##################################################################################################################
