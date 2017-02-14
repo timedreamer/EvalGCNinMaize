2557ae7782f0d5f92b364e28f9b4595315f0861b
JHuang <hjraikkonen@gmail.com>
2017-02-14 13:59:43 -0500

    add corrplot

--- src/plot_heatmap.R
+++ src/plot_heatmap.R
@@ -1,23 +1,47 @@
 library(pheatmap)
+library("corrplot")
 
-setwd("D:\\Users\\jhuang\\Desktop")
-
-t1 <- read.csv("t1.csv")
-t1 <- t1[,-1]
-
-t2 <- read.table("maize_cpm_1266_allGOMatrix.txt",sep="\t",header=T)
-row.names(t2) <- t2[,1]
-t2 <- t2[,-1]
+setwd("C:\\WORK\\EvalGCNinMaize\\data\\AUC_rank\\maize\\methods")
+vst <- read.table("maize_vst_1266_allGOMatrix.txt",sep="\t",header=T)
+row.names(vst) <- vst[,1]
+vst <- vst[,-1]
 
 # save as svg, 700*400
-pheatmap(t2,scale = "row",cutree_rows = 3,show_rownames = F,
-         cellwidth = 20,treeheight_row =0,treeheight_col = 20,main="cpm_GO")
+pheatmap(vst,scale = "row",show_rownames = F,cutree_rows = 3,
+         cellwidth = 20,treeheight_row =0,treeheight_col = 20,main="VST_GO")
 
-t2 <- read.table("maize_cpm_1266_allPPPTYMatrix_nullDelete.txt",sep="\t",header=T)
+t2 <- read.table("maize_vst_1266_allPPPTYMatrix__nullDelete.txt",sep="\t",header=T)
 row.names(t2) <- t2[,1]
 t2 <- t2[,-1]
 pheatmap(t2,scale = "row",cutree_rows = 3,show_rownames = F,fontsize_col =20,
-         cellwidth = 20,treeheight_row =0,treeheight_col = 20,main="cpm_PPPTY")
+         cellwidth = 20,treeheight_row =0,treeheight_col = 20,main="RPKM_PPPTY")
+
+t2 <- read.table("maize_rpkm_1266_allGOMatrix.txt",sep="\t",header=T)
+row.names(t2) <- t2[,1]
+t2 <- t2[,-1]
+t <- stack(t2)
+pairwise.wilcox.test(t[,1],t[,2],p.adjust.method = "b",alternative="g")
 
 
-cogene_result["GRMZM2G016890"]
+fd <- "C:\\Users\\jhuang\\OneDrive\\Network\\FROM3077Windows\\Figures\\final_AUC\\arabidopsis"
+
+setwd(fd)
+
+t2 <- read.table("arab_GOMatrix.txt",sep="\t",header = T)
+wilcox.test(t2$X1363pcc,t2$X1363clr,paired = T,alternative = 't',conf.int = T,exact = T)
+apply(X = t2[,-1],2, mean)
+colnames(t2)
+t2[1:5,1:5]
+
+
+
+
+get_lower_tri<-function(cormat){
+  cormat[upper.tri(cormat)] <- NA
+  return(cormat)
+}
+
+
+ggplot(corData, aes(x=Var1, y=Var2, fill=value)) +geom_tile() + xlab("") + ylab("") + 
+  scale_fill_gradient2(low = "navyblue", high = "red", mid = "white",midpoint = 0.5, limit = c(0,1), space = "Lab") + 
+  theme(axis.text.x=element_text(angle = -90, hjust = 0)) # export as pd
